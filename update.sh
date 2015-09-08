5fb76346ecfc6fd312abaff3fd32f925e70d60f3
Diego Biavati <diegobiavati@gmail.com>
2015-09-08 08:03:18 -0300

    Add function for update submodule plugins

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+echo "Update plugins .vim"
+cd ~/.vim
+git submodule foreach git pull origin master
+git commit -m "Update plugins" -a
+cd ~
+
 echo "Update apt-get"
 sudo apt-get update -y
 echo "Upgrade apt-get"
