6a5875fedefb31f03bedaf42f73074a022087e4a
Diego Biavati <diegobiavati@gmail.com>
2017-01-16 12:30:45 -0200

    update plugins and add install file

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-echo "ok"
+echo "Instalação " (date)
 
-sudo apt-get update
-sudo apt-get dist-upgrade -y
+echo "1 - Atualizando Repositórios"
+sudo apt update -qq
+echo "2 - Atualizando Sistema"
+sudo apt full-upgrade -y -qq
+echo "3 - Instalando Aplicações"
+sudo apt install --quiet curl build-essential git-core vim-nox exuberant-ctags ncurses-term vim-gtk ack-grep zsh fortune screenfetch
+
+sudo apt autoremove
+sudo apt autoclean
+
